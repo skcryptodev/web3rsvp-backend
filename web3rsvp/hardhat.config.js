f6a2382c7e13aeb6a7fc48d916cf5afabba140aa
Dev99 <skcryptodev@gmail.com>
2022-08-09 16:40:05 -0400

    section 4

--- web3rsvp/hardhat.config.js
+++ web3rsvp/hardhat.config.js
@@ -1,6 +1,17 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config()
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.9",
+  networks: {
+    localhost: {
+      url: "http://127.0.0.1:8545/",
+      chainId: 31337
+    },
+    mumbai: {
+      url: process.env.STAGING_INFURA_URL,
+      accounts: [`0x${process.env.STAGING_PRIVATE_KEY}`],
+    }
+  }
 };
